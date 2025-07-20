eb7a529c7a78dd393a16b6d9f9c1684e7aafdd42
nnippon99 <adamayyad1999@hotmail.com>
2025-07-20 13:16:41 +0300

    chime: Init PixelOS!
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>

--- aosp.mk
+++ aosp.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit some common Halcyon stuff.
-$(call inherit-product, vendor/halcyon/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Lineage Health
 PRODUCT_PACKAGES += \
@@ -15,3 +15,10 @@ $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
+# Inherit more stuff
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
