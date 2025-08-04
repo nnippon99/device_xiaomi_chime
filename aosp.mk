98b23ce4a6e4256f57004d9810df4c625392dc88
nnippon99 <adamayyad1999@hotmail.com>
2025-08-04 09:55:24 -0500

    chime: fixes
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>

--- aosp.mk
+++ aosp.mk
@@ -12,6 +12,9 @@ PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
 
 # Inherit more stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -22,7 +25,9 @@ TARGET_SUPPORTS_QUICK_TAP := true
 
 # Speed Profiles
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := \
+    frameworks/base/boot/boot-image-profile.txt \
+    frameworks/base/boot/boot-image-profile-extra.txt
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Enable whole-program R8 Java optimizations for SystemUI and system_server,
